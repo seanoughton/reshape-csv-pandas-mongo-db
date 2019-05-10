ff1c3c7ff43c45b43fb89508a63053d9843e0ef9
seanoughton <seanoughton@gmail.com>
2019-05-10 16:13:54 -0700

    read in csv, converted to json, inserted into mongo db

--- panads-to-mongodb/import_csv_to_mongo.py
+++ panads-to-mongodb/import_csv_to_mongo.py
@@ -16,7 +16,10 @@ def import_content(filepath):
     cdir = os.path.dirname(__file__)
     file_res = os.path.join(cdir, filepath)
 
-
+    data = pd.read_csv(file_res)
+    data_json = json.loads(data.to_json(orient='records'))
+    db_cm.remove()
+    db_cm.insert(data_json)
 
 if __name__ == "__main__":
   filepath = 'zoo.csv'  #// pass csv file path
