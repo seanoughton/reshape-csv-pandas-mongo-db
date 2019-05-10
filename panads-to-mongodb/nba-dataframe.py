34a2587a08ae6268d69c2d3f80130e400fec9af7
seanoughton <seanoughton@gmail.com>
2019-05-10 16:16:52 -0700

    played around with adding in columns and changing column values

--- panads-to-mongodb/nba-dataframe.py
+++ panads-to-mongodb/nba-dataframe.py
@@ -7,6 +7,12 @@ import json
 
 data = pd.read_csv('./csv/nba.csv')
 
+data['sport']  = 'basketball'
+data["Weight in Kg"] = data["Weight"].mul(0.5)
+data.dropna(subset=["Salary"], inplace=True)
+
+data['Salary'].fillna(value=0,inplace=True)
+data['College'].fillna(value='No College',inplace=True)
 
 
 print( data.dropna(how='all').rank() )
